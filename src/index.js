8e65b036288ebba179e5a9cf54b273778536be6d
leah <leahtiktin@gmail.com>
2024-05-19 19:29:03 -0600

    import from drop-down module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import './style.css';
-import { createDropDownEvents } from './drop-down.js';
+import createDropDownEvents from '@leah205/dynamic-dropdown/index.js';
 
-createDropDownEvents();
+const navGroupTitles = Array.from(document.querySelectorAll('.drop-down > li'));
+createDropDownEvents(navGroupTitles);
+
+/*function createDropDownEvents(titles) {
+    titles.forEach((title) => {
+        title.addEventListener('mouseover', () => {
+            showDropDown(title);
+        });
+        title.parentNode.addEventListener('mouseleave', () => {
+            hideDropDown(title);
+        });
+    });
+}
+
+function showDropDown(title) {
+    const dropDownList = title.nextElementSibling;
+    dropDownList.classList.remove('hidden');
+}
+
+function hideDropDown(title) {
+    const dropDownList = title.nextElementSibling;
+    dropDownList.classList.add('hidden');
+}
+
+export { createDropDownEvents };*/
